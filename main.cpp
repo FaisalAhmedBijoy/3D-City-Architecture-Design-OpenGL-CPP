e83290c444bf5d5f6a8eafeb2c578786394a4dd9
Faisal Ahmed <47251096+FaisalAhmedBijoy@users.noreply.github.com>
2021-05-23 00:33:03 +0600

    Update main.cpp
    Serialize the shop, light, sign

--- main.cpp
+++ main.cpp
@@ -14,11 +14,6 @@ using namespace std;
 
 const int window_width = 1200;
 const int window_height = 750;
-int mx,my;
-
-GLfloat eye[] = {0,40,50};
-GLfloat look[] = {0,40,-500};
-
 GLfloat eyeX=0;
 GLfloat eyeY=40;
 GLfloat eyeZ=50;
@@ -27,12 +22,9 @@ GLfloat lookX=0;
 GLfloat lookY=40;
 GLfloat lookZ=-500;
 
-
-//float nipa_z = 10,nipa_x = 0;
-//float theta_right = 0, theta_left=0, theta =0;
-//bool leg_rot_x = 0,leg_rot_y = 0,leg_rot_z = 0;
-
-int b_rot = 0;
+bool light_switch_0=true;
+bool light_switch_1=true;
+bool spot_light_switch=true;
 
 
 float rot = 0;
@@ -59,9 +51,9 @@ static GLubyte c_ind[6][4] =
 
     {3,1,5,7}, //front
     {2,0,1,3}, //left
-    {7,5,4,6}, //r
+    {7,5,4,6}, //right
     {2,3,7,6}, //top
-    {1,0,5,4}, //bot
+    {1,0,5,4}, //bottom
     {6,4,0,2}, //back
 
 
@@ -89,7 +81,6 @@ void material_property(float R, float G, float B)
 {
     GLfloat no_mat[] = { 0.0, 0.0, 0.0, 1.0 };
     GLfloat mat_ambient[] = { R, G, B, 1.0 };
-    //GLfloat mat_ambient[] = { 0, 0, 0, 1.0 };
     GLfloat mat_diffuse[] = { R, G, B, 1.0 };
     GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
     GLfloat mat_shininess[] = {255};
@@ -110,20 +101,12 @@ void cube(float R=0.5, float G=0.5, float B=0.5 )
     glBegin(GL_QUADS);
     for (GLint i = 0; i <6; i++)
     {
-        //        glColor3f(R,G,B);
-        //
-        //        R += 0.05;
-        //        G += 0.05;
-        //        B += 0.05;
+
         getNormal3p(v_cube[c_ind[i][0]][0], v_cube[c_ind[i][0]][1], v_cube[c_ind[i][0]][2],
                     v_cube[c_ind[i][1]][0], v_cube[c_ind[i][1]][1], v_cube[c_ind[i][1]][2],
                     v_cube[c_ind[i][2]][0], v_cube[c_ind[i][2]][1], v_cube[c_ind[i][2]][2]);
 
-//        for (GLint j=0; j<4; j++)
-//        {
-//            glVertex3fv(&v_cube[c_ind[i][j]][0]) ;
-//            glTexCoord2f(1,1);
-//        }
+
         glTexCoord2f(1,1);
         glVertex3fv(&v_cube[c_ind[i][0]][0]);
         glTexCoord2f(1,0);
@@ -293,6 +276,20 @@ void road()
 
     glPushMatrix();
     glTranslatef(0,0.5,20);
+    glScalef(300,1,10);
+    glTranslatef(-0.5,-0.5,-0.5);
+    cube();
+    glPopMatrix();
+
+    glDisable(GL_TEXTURE_2D);
+
+    // left side road
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D,10);
+
+    glPushMatrix();
+    glTranslatef(-50,0.5,0);
+    glRotatef(90,0,1,0);
     glScalef(200,1,10);
     glTranslatef(-0.5,-0.5,-0.5);
     cube();
@@ -300,14 +297,14 @@ void road()
 
     glDisable(GL_TEXTURE_2D);
 
-    //side road
+    //right side road
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D,10);
 
     glPushMatrix();
-    glTranslatef(-50,0.5,0);
+    glTranslatef(130,0.5,0);
     glRotatef(90,0,1,0);
-    glScalef(100,1,10);
+    glScalef(200,1,10);
     glTranslatef(-0.5,-0.5,-0.5);
     cube();
     glPopMatrix();
@@ -378,7 +375,7 @@ void wood_leaf()
 
 }
 
-void traffic_light()
+void traffic_signal()
 {
     // base
     glEnable(GL_TEXTURE_2D);
@@ -406,7 +403,6 @@ void traffic_light()
     glDisable(GL_TEXTURE_2D);
 
     // roof off
-    //roof
     glPushMatrix();
     glTranslatef(-20,55,0);
     glScalef(5,2,5);
@@ -444,7 +440,6 @@ void signboard()
     glDisable(GL_TEXTURE_2D);
 
     // roof off
-    //roof
     glPushMatrix();
     glTranslatef(-50,55,0);
     glScalef(5,2,5);
@@ -478,6 +473,46 @@ void shop()
     glPopMatrix();
 }
 
+void road_light()
+{
+    // base
+    glPushMatrix();
+
+    glPushMatrix();
+    glTranslatef(80,30,0);
+    glScalef(1,30,1);
+    glTranslatef(-0.5,-0.5,-0.5);
+    cube(0.1,0.1,0.1);
+    glPopMatrix();
+
+    // light stand
+    glPushMatrix();
+    glTranslatef(85,42,0);
+    glScalef(10,1,1);
+    glTranslatef(-0.5,-0.5,-0.5);
+    cube(0.8,0.6,0.2);
+    glPopMatrix();
+
+    // Bulb line
+    glPushMatrix();
+    glTranslatef(85,38,0);
+    glScalef(1,7,1);
+    glTranslatef(-0.5,-0.5,-0.5);
+    cube(0.9,0.9,0.9);
+    glPopMatrix();
+
+    // texture light glutsolidshere
+
+
+    glPushMatrix();
+    glTranslatef(85,35,0);
+    glutSolidSphere(2,16,16);
+    glPopMatrix();
+
+    glPopMatrix();
+
+}
+
 void _light()
 {
     //GLfloat no_light[] = { 0.0, 0.0, 0.0, 1.0 };
@@ -493,11 +528,101 @@ void _light()
     glLightfv( GL_LIGHT7, GL_SPECULAR, light_specular);
 
     glLightfv( GL_LIGHT7, GL_POSITION, light_position);
+}
+
+void light_function_0(float x, float y, float z)
+{
+    // Light Specification
+    GLfloat no_light[] = { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat light_ambient[] = {0.1, 0.1, 0.1, 1.0};
+    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1 };
+    GLfloat light_specular[] = { 1, 1, 1, 1 };
+    GLfloat light_position[] = { x, y, z, 1.0 };
+
+    glEnable( GL_LIGHT0);
+    if (light_switch_0)
+    {
+        glLightfv( GL_LIGHT0, GL_AMBIENT, light_ambient);
+        glLightfv( GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+        glLightfv( GL_LIGHT0, GL_SPECULAR, light_specular);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT0, GL_AMBIENT, no_light);
+        glLightfv( GL_LIGHT0, GL_DIFFUSE, no_light);
+        glLightfv( GL_LIGHT0, GL_SPECULAR, no_light);
+
+    }
 
 
-    /*GLfloat spot_direction[] = { 0.0, -1.0, 0.0 };
-    glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, spot_direction);
-    glLightf( GL_LIGHT0, GL_SPOT_CUTOFF, 10.0);*/
+    glLightfv( GL_LIGHT0, GL_POSITION, light_position);
+
+}
+
+void light_function_1(float x, float y, float z)
+{
+    // Light Specification
+    GLfloat no_light[] = { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat light_ambient[] = {0.1, 0.1, 0.1, 1.0};
+    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1 };
+    GLfloat light_specular[] = { 1, 1, 1, 1 };
+    GLfloat light_position[] = { x, y, z, 1.0 };
+
+    glEnable( GL_LIGHT1);
+    if (light_switch_1)
+    {
+        glLightfv( GL_LIGHT1, GL_AMBIENT, light_ambient);
+        glLightfv( GL_LIGHT1, GL_DIFFUSE, light_diffuse);
+        glLightfv( GL_LIGHT1, GL_SPECULAR, light_specular);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT1, GL_AMBIENT, no_light);
+        glLightfv( GL_LIGHT1, GL_DIFFUSE, no_light);
+        glLightfv( GL_LIGHT1, GL_SPECULAR, no_light);
+
+    }
+
+
+    glLightfv( GL_LIGHT1, GL_POSITION, light_position);
+
+}
+
+
+void spot_light_function(float x, float y, float z)
+{
+    // Light Specification
+    GLfloat no_light[] = { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat light_ambient[] = {0.0, 0.5, 0.0, 1.0};
+    GLfloat light_diffuse[] = { 0.0, 1.0, 0.0, 1 };
+    GLfloat light_specular[] = { 1, 1, 1, 1 };
+    GLfloat light_position[] = { x, y, z, 1.0 };
+
+    glEnable( GL_LIGHT2);
+    if (spot_light_switch)
+    {
+        glLightfv( GL_LIGHT2, GL_AMBIENT, light_ambient);
+        glLightfv( GL_LIGHT2, GL_DIFFUSE, light_diffuse);
+        glLightfv( GL_LIGHT2, GL_SPECULAR, light_specular);
+
+    }
+    else
+    {
+        glLightfv( GL_LIGHT2, GL_AMBIENT, no_light);
+        glLightfv( GL_LIGHT2, GL_DIFFUSE, no_light);
+        glLightfv( GL_LIGHT2, GL_SPECULAR, no_light);
+
+    }
+
+
+    glLightfv( GL_LIGHT2, GL_POSITION, light_position);
+    GLfloat direction[]= {0,-1,0,1};
+    GLfloat cut_off=60;
+    glLightfv(GL_LIGHT2,GL_SPOT_DIRECTION,direction);
+    glLightf(GL_LIGHT2,GL_SPOT_CUTOFF,cut_off);
+
 }
 void axes()
 {
@@ -532,13 +657,9 @@ static void key(unsigned char key, int x, int y)
 {
     switch (key)
     {
-    //for light
-
-
     case 'u': // up
 
-        //eye[1]++;
-        //look[1]++;
+
         eyeY++;
         lookY++;
 
@@ -547,16 +668,14 @@ static void key(unsigned char key, int x, int y)
 
         eyeY--;
         lookY--;
-        //eye[1]--;
-        //look[1]--;
         break;
     case 'a': // look right
         lookX++;
-        //look[0]++;
+
         break;
     case 'b':
         lookX--;
-        //look[0]--; // look left
+        // look left
         break;
 
     case 'p': // rotate left
@@ -569,8 +688,7 @@ static void key(unsigned char key, int x, int y)
 
         eyeX++;
         lookX++;
-        //eye[0]++;
-        //look[0]++;
+
 
         break;
     case 'r': // right
@@ -578,18 +696,24 @@ static void key(unsigned char key, int x, int y)
         eyeX--;
         lookX--;
 
-        //eye[0]--;
-        //look[0]--;
-
         break;
     case '+': // zoom in
-        //eye[2]--;
         eyeZ--;
         break;
 
     case '-':
-        //eye[2]++; // zoom out
+        //zoom out
         eyeZ++;
+        break;
+
+    case '1':
+        light_switch_0 =! light_switch_0;
+        break;
+    case '2':
+        light_switch_1 =! light_switch_1;
+        break;
+    case '3':
+        spot_light_switch =! spot_light_switch;
         break;
     }
 
@@ -597,16 +721,13 @@ static void key(unsigned char key, int x, int y)
 }
 
 
-
-
-static void display(void)
+void display_settings()
 {
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    int lim = 8;
-    glFrustum(-lim, lim, -lim, lim, 4, 200);
+    int frustum_window = 8;
+    glFrustum(-frustum_window, frustum_window, -frustum_window, frustum_window, 4, 200);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -616,20 +737,117 @@ static void display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity() ;
 
-    //gluLookAt(eyeX,eyeY,eyeZ, lookX,lookY,lookZ, 0,1,0);
     gluLookAt(eyeX,eyeY,eyeZ, lookX,lookY,lookZ, 0,1,0);
     //glViewport(0, 0, window_width, window_height);
 
     glRotatef(rot, 0,1,0);
 
+}
+
+static void display(void)
+{
+
+
+    display_settings();
     axes();
-    //buiding();
+    buiding();
     road();
     //trees();
-    wood_leaf();
-    traffic_light();
+
+    // Tree 1
+
+    for (int i=10; i<=100; i+=20)
+    {
+        glPushMatrix();
+        glTranslatef(0,-20,-i);
+        wood_leaf();
+        glPopMatrix();
+
+    }
+
+    // Tree 2
+
+    for (int i=10; i<=100; i+=20)
+    {
+        glPushMatrix();
+        glTranslatef(45,-20,-i);
+        wood_leaf();
+        glPopMatrix();
+
+    }
+
+    //wood_leaf();
+
+    // Traffic Signal 1
+    glPushMatrix();
+    glTranslatef(-10,-20,10);
+    traffic_signal();
+    glPopMatrix();
+
+    // Traffic Signal 2
+    glPushMatrix();
+    glTranslatef(140,-20,10);
+    traffic_signal();
+    glPopMatrix();
+
+
+
+    // Sign board 1
+    glPushMatrix();
+    glTranslatef(-25,-10,-20);
     signboard();
+    glPopMatrix();
+
+     // Sign board 2
+    glPushMatrix();
+    glTranslatef(200,-10,-20);
+    signboard();
+    glPopMatrix();
+
+
+
+
     shop();
+
+
+    //light 1
+    glPushMatrix();
+    light_function_0(40,35,0);
+    glTranslatef(40,-20,0);
+    road_light();
+    //cube();
+    glPopMatrix();
+
+    // light 2
+    glPushMatrix();
+    light_function_1(-100,35,0);
+    glTranslatef(-100,0,0);
+    //road_light();
+    //cube();
+    glPopMatrix();
+
+    for (int i=10; i<=100; i=i+20)
+    {
+        glPushMatrix();
+        light_function_1(-100,35,0);
+        glTranslatef(40,-20,-i);
+        road_light();
+        //cube();
+        glPopMatrix();
+    }
+
+    for (int i=10; i<=100; i=i+20)
+    {
+        glPushMatrix();
+        light_function_1(-150,35,0);
+        glTranslatef(-150,-20,-i);
+        road_light();
+        //cube();
+        glPopMatrix();
+    }
+
+
+
 
     glFlush();
     glutSwapBuffers();
@@ -645,7 +863,7 @@ int main(int argc, char *argv[])
     glutCreateWindow("3D City");
 
     LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\building1.bmp",1);
-    LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\building2.bmp",2);
+    LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\building7.bmp",2);
     LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\building3.bmp",3);
     LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\building4.bmp",4);
     LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\building5.bmp",5);
@@ -670,8 +888,35 @@ int main(int argc, char *argv[])
     LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\wood1.bmp",13);
     //LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\wood2.bmp",15);
     LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\leaf1.bmp",14);
-    LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\leaf2.bmp",15);
 
+    // road light bulb texture
+    LoadTexture("C:\\Users\\Shimul\\Documents\\CSE 4208 Computer Graphics\\City 3D\\images\\bulb4.bmp",15);
+
+
+    cout<<"--------------------------"<<endl;
+    cout<<"--------------------------"<<endl;
+    cout<<"-- 3D City Design ---"<<endl;
+    cout<<"-- Faisal Ahmed ----------"<<endl;
+    cout<<"-- Roll: 1607048 ---------"<<endl;
+    cout<<"--------------------------"<<endl;
+    cout<<"--------------------------"<<endl;
+    cout<<"Press : u -> Move Up"<<endl;
+    cout<<"Press : d -> Move Down"<<endl;
+    cout<<"Press : l -> Move Left"<<endl;
+    cout<<"Press : r -> Move Right"<<endl;
+
+    cout<<"Press : p -> Rotate Right"<<endl;
+    cout<<"Press : p -> Rotate Right"<<endl;
+
+    cout<<"Press : a -> Look Right"<<endl;
+    cout<<"Press : b -> Look Left"<<endl;
+
+    cout<<"Press : + -> Zoom In"<<endl;
+    cout<<"Press : - -> Zoom Out"<<endl;
+
+    cout<<"Press : 1 -> ON/OFF Light 1"<<endl;
+    cout<<"Press : 2 -> ON/OFF Light 2"<<endl;
+    cout<<"Press : 3 -> ON/OFF Spot Light"<<endl;
 
 
     glutDisplayFunc(display);
